27163de6caa0ef71bc083abfb917372398c75a06
Adam Lee <awesomejm213@gmail.com>
2019-01-07 10:49:40 -0500

    codet

--- js/pocket-calculator.js
+++ js/pocket-calculator.js
@@ -1,3 +1,52 @@
+
 function button1() {
-alert("sss")
+  var p = document.getElementById("display");
+  p.innerHTML += "1";
 }
+
+function button2() {
+  var p = document.getElementById("display");
+  p.innerHTML += "2";
+}
+function button3() {
+  var p = document.getElementById("display");
+  p.innerHTML += "3";
+}
+
+function button4() {
+  var p = document.getElementById("display");
+  p.innerHTML += "4";
+}
+
+function button5() {
+  var p = document.getElementById("display");
+  p.innerHTML += "5";
+}
+
+function button6() {
+  var p = document.getElementById("display");
+  p.innerHTML += "6";
+}
+
+function button7() {
+  var p = document.getElementById("display");
+  p.innerHTML += "7";
+}
+
+function button8() {
+  var p = document.getElementById("display");
+  p.innerHTML += "8";
+}
+
+function button9() {
+  var p = document.getElementById("display");
+  p.innerHTML += "9";
+}
+
+function button0() {
+  var p = document.getElementById("display");
+  p.innerHTML += "0";
+  numbers.push(0)
+}
+var numbers = []
+console.log(numbers);
