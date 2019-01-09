fcd6f55bc78fee714d039218420e1dc82772db1c
Adam Lee <awesomejm213@gmail.com>
2019-01-09 10:45:53 -0500

    no

--- js/pocket-calculator.js
+++ js/pocket-calculator.js
@@ -1,52 +1,57 @@
+var number1 = [];
+var number2 = [];
+var sign = []
+// function button1() {
+//   var p = document.getElementById("display");
+//   p.innerHTML += "1";
+// }
+//
+// function button2() {
+//   var p = document.getElementById("display");
+//   p.innerHTML += "2";
+// }
+// function button3() {
+//   var p = document.getElementById("display");
+//   p.innerHTML += "3";
+// }
+//
+// function button4() {
+//   var p = document.getElementById("display");
+//   p.innerHTML += "4";
+// }
+//
+// function button5() {
+//   var p = document.getElementById("display");
+//   p.innerHTML += "5";
+// }
+//
+// function button6() {
+//   var p = document.getElementById("display");
+//   p.innerHTML += "6";
+// }
+//
+// function button7() {
+//   var p = document.getElementById("display");
+//   p.innerHTML += "7";
+// }
+//
+// function button8() {
+//   var p = document.getElementById("display");
+//   p.innerHTML += "8";
+// }
+//
+// function button9() {
+//   var p = document.getElementById("display");
+//   p.innerHTML += "9";
+// }
+//
+// function button0() {
+//   var p = document.getElementById("display");
+//   p.innerHTML += "0";
+//   numbers.push(0)
+// }
+function bPress(x){
 
-function button1() {
-  var p = document.getElementById("display");
-  p.innerHTML += "1";
+  
 }
-
-function button2() {
-  var p = document.getElementById("display");
-  p.innerHTML += "2";
-}
-function button3() {
-  var p = document.getElementById("display");
-  p.innerHTML += "3";
-}
-
-function button4() {
-  var p = document.getElementById("display");
-  p.innerHTML += "4";
-}
-
-function button5() {
-  var p = document.getElementById("display");
-  p.innerHTML += "5";
-}
-
-function button6() {
-  var p = document.getElementById("display");
-  p.innerHTML += "6";
-}
-
-function button7() {
-  var p = document.getElementById("display");
-  p.innerHTML += "7";
-}
-
-function button8() {
-  var p = document.getElementById("display");
-  p.innerHTML += "8";
-}
-
-function button9() {
-  var p = document.getElementById("display");
-  p.innerHTML += "9";
-}
-
-function button0() {
-  var p = document.getElementById("display");
-  p.innerHTML += "0";
-  numbers.push(0)
-}
-var numbers = []
 console.log(numbers);
