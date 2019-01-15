ff70cfd4e47131c0d45c8880c7f0c1909798e50b
Adam Lee <awesomejm213@gmail.com>
2019-01-15 10:48:20 -0500

    good work day

--- js/pocket-calculator.js
+++ js/pocket-calculator.js
@@ -1,58 +1,27 @@
-var display = [];
+var display = [0];
+var numberpast = [];
 var number = [0];
-var sign = []
-// function button1() {
-//   var p = document.getElementById("display");
-//   p.innerHTML += "1";
-// }
-//
-// function button2() {
-//   var p = document.getElementById("display");
-//   p.innerHTML += "2";
-// }
-// function button3() {
-//   var p = document.getElementById("display");
-//   p.innerHTML += "3";
-// }
-//
-// function button4() {
-//   var p = document.getElementById("display");
-//   p.innerHTML += "4";
-// }
-//
-// function button5() {
-//   var p = document.getElementById("display");
-//   p.innerHTML += "5";
-// }
-//
-// function button6() {
-//   var p = document.getElementById("display");
-//   p.innerHTML += "6";
-// }
-//
-// function button7() {
-//   var p = document.getElementById("display");
-//   p.innerHTML += "7";
-// }
-//
-// function button8() {
-//   var p = document.getElementById("display");
-//   p.innerHTML += "8";
-// }
-//
-// function button9() {
-//   var p = document.getElementById("display");
-//   p.innerHTML += "9";
-// }
-//
-// function button0() {
-//   var p = document.getElementById("display");
-//   p.innerHTML += "0";
-//   numbers.push(0)
-// }
+let numbercount = 0
+var sign = [];
+
 function numbuttonPress(x){
   var p = document.getElementById("display");
   p.innerHTML += x;
+  number.push(x);
+  display.push(x);
+  console.log(number);
+  numbercount = numbercount + 1;
+console.log(numbercount);
 
+    if (number[0] == 0){
+      p.innerHTML = x;
+      number.shift();
+  }
 }
-console.log(numbers);
+function functionbutton(x){
+  alert("sjsjjsdjsjsjks")
+}
+if (numbercount%3==0){
+  var o = document.getElementById("display");
+  o.innerHTML += 1;
+}
