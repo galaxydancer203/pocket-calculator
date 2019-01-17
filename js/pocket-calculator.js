3f5a68fb4a4f611f6fc4272c6bd3dec57ffd803c
Adam Lee <awesomejm213@gmail.com>
2019-01-17 10:50:28 -0500

    ss

--- js/pocket-calculator.js
+++ js/pocket-calculator.js
@@ -1,27 +1,41 @@
 var display = [0];
 var numberpast = [];
 var number = [0];
-let numbercount = 0
+let numbercount = 0;
 var sign = [];
 
 function numbuttonPress(x){
   var p = document.getElementById("display");
+
+
   p.innerHTML += x;
   number.push(x);
   display.push(x);
-  console.log(number);
   numbercount = numbercount + 1;
-console.log(numbercount);
+  String(number);
+console.log(typeof number);
+console.log(number)
 
-    if (number[0] == 0){
-      p.innerHTML = x;
-      number.shift();
-  }
+if (number[0] == 0){
+  number.shift();
+  p.innerHTML = x;
+
+}
+
+  console.log(numbercount);
+// number=number.toLocaleString('en');
 }
 function functionbutton(x){
-  alert("sjsjjsdjsjsjks")
+  // alert("sjsjjsdjsjsjks")
+  // JSON.stringify(number)
+  console.log(number)
+  console.log(typeof number);
 }
+
+
+
+
 if (numbercount%3==0){
-  var o = document.getElementById("display");
-  o.innerHTML += 1;
+  var p = document.getElementById("display");
+  // p.innerHTML += 1;
 }
